a5c56ba77af4e463e7dd8858fe10c6f1ee7c6764
blaziter <petrblaztran@gmail.com>
2024-02-29 20:26:52 +0100

    feat(components): add more components and generate with new color

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import { DayPicker } from 'react-day-picker';
 
-import { cn } from '@lib/utils';
+import { cn } from '@lib/utils/utils';
 import { buttonVariants } from '@components/ui/button';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
@@ -32,10 +32,10 @@ function Calendar({
                 table: 'w-full border-collapse space-y-1',
                 head_row: 'flex',
                 head_cell:
-                    'text-slate-500 rounded-md w-8 font-normal text-[0.8rem] dark:text-slate-400',
+                    'text-gray-500 rounded-md w-8 font-normal text-[0.8rem] dark:text-gray-400',
                 row: 'flex w-full mt-2',
                 cell: cn(
-                    'relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-slate-100 [&:has([aria-selected].day-outside)]:bg-slate-100/50 [&:has([aria-selected].day-range-end)]:rounded-r-md dark:[&:has([aria-selected])]:bg-slate-800 dark:[&:has([aria-selected].day-outside)]:bg-slate-800/50',
+                    'relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-gray-100 [&:has([aria-selected].day-outside)]:bg-gray-100/50 [&:has([aria-selected].day-range-end)]:rounded-r-md dark:[&:has([aria-selected])]:bg-gray-800 dark:[&:has([aria-selected].day-outside)]:bg-gray-800/50',
                     props.mode === 'range'
                         ? '[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md'
                         : '[&:has([aria-selected])]:rounded-md'
@@ -47,22 +47,22 @@ function Calendar({
                 day_range_start: 'day-range-start',
                 day_range_end: 'day-range-end',
                 day_selected:
-                    'bg-slate-900 text-slate-50 hover:bg-slate-900 hover:text-slate-50 focus:bg-slate-900 focus:text-slate-50 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50 dark:hover:text-slate-900 dark:focus:bg-slate-50 dark:focus:text-slate-900',
+                    'bg-gray-900 text-gray-50 hover:bg-gray-900 hover:text-gray-50 focus:bg-gray-900 focus:text-gray-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50 dark:hover:text-gray-900 dark:focus:bg-gray-50 dark:focus:text-gray-900',
                 day_today:
-                    'bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-50',
+                    'bg-gray-100 text-gray-900 dark:bg-gray-800 dark:text-gray-50',
                 day_outside:
-                    'day-outside text-slate-500 opacity-50  aria-selected:bg-slate-100/50 aria-selected:text-slate-500 aria-selected:opacity-30 dark:text-slate-400 dark:aria-selected:bg-slate-800/50 dark:aria-selected:text-slate-400',
-                day_disabled: 'text-slate-500 opacity-50 dark:text-slate-400',
+                    'day-outside text-gray-500 opacity-50  aria-selected:bg-gray-100/50 aria-selected:text-gray-500 aria-selected:opacity-30 dark:text-gray-400 dark:aria-selected:bg-gray-800/50 dark:aria-selected:text-gray-400',
+                day_disabled: 'text-gray-500 opacity-50 dark:text-gray-400',
                 day_range_middle:
-                    'aria-selected:bg-slate-100 aria-selected:text-slate-900 dark:aria-selected:bg-slate-800 dark:aria-selected:text-slate-50',
+                    'aria-selected:bg-gray-100 aria-selected:text-gray-900 dark:aria-selected:bg-gray-800 dark:aria-selected:text-gray-50',
                 day_hidden: 'invisible',
                 ...classNames,
             }}
             components={{
-                IconLeft: (/** { ...props } */) => (
+                IconLeft: ({ ...props }) => (
                     <ChevronLeftIcon className="h-4 w-4" />
                 ),
-                IconRight: (/** { ...props } */) => (
+                IconRight: ({ ...props }) => (
                     <ChevronRightIcon className="h-4 w-4" />
                 ),
             }}
